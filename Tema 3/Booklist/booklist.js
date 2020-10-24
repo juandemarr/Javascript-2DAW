f52bf05a9bee54b8cbb6727ef3858bf5acc53ae3
juande <cloud15jc@gmail.com>
2020-10-24 20:50:22 +0200

    en métodos de booklist

--- Tema 3/Booklist/booklist.js
+++ Tema 3/Booklist/booklist.js
@@ -3,42 +3,44 @@
 * Create another class called Book
 
 * BookLists should have the following properties:
-    Number of books marked as read
-    Number of books marked not read yet
-    A reference to the next book to read (book object)
-    A reference to the current book being read (book object)
-    A reference to the last book read (book object)
-    An array of all the Books
+  x Number of books marked as read
+  x Number of books marked not read yet
+  x A reference to the next book to read (book object)
+  x A reference to the current book being read (book object)
+  x A reference to the last book read (book object)
+  x An array of all the Books
 * Each Book should have several properties:
-    Title
-    Genre
-    Author
-    Read (true or false)
-    Read date, can be blank, otherwise needs to be a JS Date() object
+  x Title
+  x Genre
+  x Author
+  x Read (true or false)
+  x Read date, can be blank, otherwise needs to be a JS Date() object
 * Every Booklist should have a few methods:
 
-.add(book)
-    should add a book to the books list.
+x   .add(book)
+        should add a book to the books list.
 .finishCurrentBook()
     should mark the book that is currently being read as "read"
-Give it a read date of new Date(Date.now())
-Change the last book read to be the book that just got finished
-Change the current book to be the next book to be read
-Change the next book to be read property to be the first unread book you find in the list of books
-* Booklists and Books might need more methods than that. Try to think of more that might be useful.*/
+    Give it a read date of new Date(Date.now())
+    Change the last book read to be the book that just got finished
+    Change the current book to be the next book to be read
+    Change the next book to be read property to be the first unread book you find in the 
+    list of books
+* Booklists and Books might need more methods than that. Try to think of more that 
+might be useful.*/
 
 class Book{
     constructor(title,genre,author){
         this.title=title;
         this.genre=genre;
         this.author=author;
-        read=null;
-        readDate=null;
+        this.read=null;
+        this.readDate=null;
     }
-    leido(){
-        read=true;
-        readDate=Date.now().toLocaleString();
-    }
+    /*get leido(){
+        this.read=true;
+        this.readDate=Date.now().toLocaleString();
+    }*/
 
     get title(){
         return this.title;
@@ -64,60 +66,72 @@ class Book{
     set author(author){
         this.author=author;
     }
-    set read(read){
-        this.read=read;
-    }
-    set readDate(readDate){
-        this.readDate=readDate;
-    }
 
 }
 
 class Booklist{
     constructor(){
-        lista=[];
+        this.listaLibros=[];
 
     }
+    
+    ///////////Miembros calculados
+
+    get numberRead(){
+        let cont=0;
+        for(let i=0; i<this.listaLibros.length; i++){
+            if(this.listaLibros[i].read==true)
+                cont++;
+        }
+        return cont;
+    }
+
+    get numberNoRead(){
+        return this.listaLibros.length-this.numberNoRead;
+    }
+
+    get nextBookRead(){
+        let bool=true;
+        let siguiente=null;
+        for(let i=0; i<this.listaLibros.length; i++){
+            if(bool && this.listaLibros[i].read==false)
+                siguiente=this.listaLibros[i];
+                bool=false;
+        }
+        return siguiente;
+    }
+
+    get currentBookRead(){
+        let current=null;
+        for(let i=0; i<this.listaLibros.length; i++){
+            if(this.listaLibros[i].read==true && this.listaLibros[i].readDate==null)
+                current=this.listaLibros[i];
+        }
+        return current;
+    }
+
+    get lastBookRead(){
+        let lastBook=null;
+        let bool=true;
+        for(let i=this.listaLibros.length-1; i>=0; i--){
+            if(bool && this.listaLibros[i].read==true)
+                lastBook=this.listaLibros[i];
+                bool=false;
+        }
+        return lastBook;
+    }
+
+    get all(){
+        return this.listaLibros;
+    }
+
+    /////////////////////////////Métodos
 
     add(title,genre,author){
-        lista.push(new Book(title,genre,author));
+        this.listaLibros.push(new Book(title,genre,author));
     }
-
-    numberRead(){
-        let cont=0;
-        for(let i=0; i<lista.length; i++){
-            if(lista[i].read==true)
-                cont++;
-        }
-        return cont;
-    }
-
-    numberNoRead(){
-        let cont=0;
-        for(let i=0; i<lista.length; i++){
-            if(lista[i].read==false)
-                cont++;
-        }
-        return cont;
-    }
-
-    nextBookRead(){
-        let bool=false;
-        for(let i=0; i<lista.length; i++){
-            if(bool==false)
-                if(lista[i].read==true)
-                    bool=true;
-        }
-        return lista[i];
-    }
-
-
 
     finishCurrentBook(){
 
     }
-
 }
-
-Booklist.add("hola","genero","autor");
-document.write();
