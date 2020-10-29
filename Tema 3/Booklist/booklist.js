0ee592ebe3b09452278b2195aada578c293555b9
juande <cloud15jc@gmail.com>
2020-10-29 18:49:07 +0100

    Booklist terminado

--- Tema 3/Booklist/booklist.js
+++ Tema 3/Booklist/booklist.js
@@ -29,6 +29,8 @@ x   .add(book)
 * Booklists and Books might need more methods than that. Try to think of more that 
 might be useful.*/
 
+
+//Considero que en el array ya pueden haber libros leidos y no leidos
 class Book{
     constructor(title,genre,author){
         this.title=title;
@@ -37,45 +39,17 @@ class Book{
         this.read=null;
         this.readDate=null;
     }
-    /*get leido(){
-        this.read=true;
-        this.readDate=Date.now().toLocaleString();
-    }*/
-
-    get title(){
-        return this.title;
-    }
-    get genre(){
-        return this.genre;
-    }
-    get author(){
-        return this.author;
-    }
-    get read(){
-        return this.read;
-    }
-    get readDate(){
-        return this.readDate;
-    }
-    set title(title){
-        this.title=title;
-    }
-    set genre(genre){
-        this.genre=genre;
-    }
-    set author(author){
-        this.author=author;
-    }
-
 }
+
 
 class Booklist{
     constructor(){
         this.listaLibros=[];
-
+        this.currentBookIndex=0;
     }
     
-    ///////////Miembros calculados
+    ////////////MIEMBROS CALCULADOS, con get. Se llaman como atributos, sin parentesis.
+    // No hay getter como en java
 
     get numberRead(){
         let cont=0;
@@ -85,53 +59,63 @@ class Booklist{
         }
         return cont;
     }
+    /*//Otra forma
+    get booksRead(){
+        return this.listaLibros.filter( (book) => book.read).length;
+    }
+    /*Ese book.read es la condicion, los que la cumplan se seleccionan en el array
+    que devuelve filter. Se pone directamente ya que book.read valdrá true o false
+    .length devuelve el número de elementos del array que filter ha generado
+    */
 
     get numberNoRead(){
-        return this.listaLibros.length-this.numberNoRead;
+        return this.listaLibros.length-this.numberRead;
     }
+    //Otra forma
+    /*get booksNoRead(){
+        return this.listaLibros.filter((book) => !book.read).length;
+    }*/
 
     get nextBookRead(){
-        let bool=true;
-        let siguiente=null;
-        for(let i=0; i<this.listaLibros.length; i++){
-            if(bool && this.listaLibros[i].read==false)
-                siguiente=this.listaLibros[i];
-                bool=false;
+        for(let i=this.currentBookIndex; i<this.listaLibros.length; i++){
+            if(this.listaLibros[i].read==false)
+                return this.listaLibros[i];
+            else
+                return null;
         }
-        return siguiente;
     }
 
     get currentBookRead(){
-        let current=null;
         for(let i=0; i<this.listaLibros.length; i++){
-            if(this.listaLibros[i].read==true && this.listaLibros[i].readDate==null)
-                current=this.listaLibros[i];
+            if(this.listaLibros[i].read==false){
+                this.currentBookIndex=i;
+                return this.listaLibros[i];
+            }else
+                return null;
         }
-        return current;
     }
 
     get lastBookRead(){
-        let lastBook=null;
-        let bool=true;
-        for(let i=this.listaLibros.length-1; i>=0; i--){
-            if(bool && this.listaLibros[i].read==true)
-                lastBook=this.listaLibros[i];
-                bool=false;
-        }
-        return lastBook;
+        if(this.currentBookIndex==0)
+            return null;
+        else
+            return this.listaLibros[this.currentBookIndex-1];
     }
 
-    get all(){
-        return this.listaLibros;
-    }
-
-    /////////////////////////////Métodos
-
-    add(title,genre,author){
-        this.listaLibros.push(new Book(title,genre,author));
+    ///////MÉTODOS
+    add(book){
+        this.listaLibros.push(book);
     }
 
     finishCurrentBook(){
-
+        this.listaLibros[this.currentBookIndex].read=true;
+        this.listaLibros[this.currentBookIndex].readDate=new Date(Date.now());
+        if(this.currentBookIndex<this.listaLibros.length)
+            this.currentBookIndex++;
+        else
+            console.log("No hay más libros para leer");
     }
+    //Date.now() devuelve los milisegundos desde el "inicio", pero pasado como parámetro
+    //a la creación del objeto Date, devuelve la fecha entera
+
 }
