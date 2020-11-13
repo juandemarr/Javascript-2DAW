894a76f244087cb16eae92f6a185ee6807d17b78
juande <cloud15jc@gmail.com>
2020-11-13 17:12:04 +0100

    DOM

---
+++ Tema 5 DOM/ej modificar dom/usoDOM.js
@@ -0,0 +1,11 @@
+/*El número de imágenes que aparecen en la página
+*Número de enlaces a redes sociales al final de la página.
+*Módifica la URL del enlace a Twitter para que te lleve a "iesayala.com"
+*Modifica la imagen del logo de la cabecera de la página ("gdi") para que aparezca 
+la imagen que tú quieras.
+*Modifica el texto que aparece en la cabecera ("Join us.....") para que se 
+muestre el texto que tu desees.*/
+console.log(document.getElementsByTagName("img"));
+document.querySelector(".social-links a[title=\"Twitter\"]").href="iesayala.com";
+document.querySelector(".navbar-left a img[alt=\"Girl Develop It logo\"]").src="https://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c4ba.png";
+document.querySelector(".homepage-full-header .container").textContent="HELLO THERE";
