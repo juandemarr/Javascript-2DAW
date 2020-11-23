c91bff5d4d946e12324beba5605e45bc8f26f18e
juande <cloud15jc@gmail.com>
2020-11-23 23:35:24 +0100

    work1 ej6 pictures viewer done

--- Tema 5 DOM/work1 ej6 visor de imagenes/work1 ej6.js
+++ Tema 5 DOM/work1 ej6 visor de imagenes/work1 ej6.js
@@ -4,5 +4,33 @@ en tu página se mostrará una imágen, junto a los botones "Siguiente" y "Anter
 Impleméntale la funcionalidad para poder navegar entre un conjunto de imágenes
 que tendrás previamente preparadas
 */
+window.onload=main;
+var img;
+var arr=['ashes.jpg','gem.jpg','visas.jpg','skarlet.jpg'];
+var num=0;
 
+function main(){
+    img=document.getElementById("imagen");
+    document.getElementById("izquierda").addEventListener("click",atras);
+    document.getElementById("derecha").addEventListener("click",siguiente);
+}
+
+function atras(){
+    if(num==0)
+        num=arr.length-1;
+    else
+        num--;
+    
+    let src="img/"+arr[num];
+    img.src=src;
+}
+function siguiente(){
+    if(num==arr.length-1)
+        num=0;
+    else
+        num++;
+
+    let src="img/"+arr[num];
+    img.src=src;
+}
 
