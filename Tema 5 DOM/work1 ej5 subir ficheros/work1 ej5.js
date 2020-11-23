7a76517cbcb97d02565f1129746acd51bff62113
juande <cloud15jc@gmail.com>
2020-11-23 19:27:47 +0100

    work1 ej5 with an error

---
+++ Tema 5 DOM/work1 ej5 subir ficheros/work1 ej5.js
@@ -0,0 +1,23 @@
+/*
+Estás diseñando una aplicación web para subir ficheros al servidor. Haciendo
+uso del DOM, consigue que el usuario pueda adjuntar tantos ficheros como 
+desee. Por tanto inicialmente será necesario un input de tipo
+fichero, un botón "Adjuntar otro fichero" y un segundo botón "Enviar", de modo
+que al pulsar el primero de los botones automáticamente aparezca otro input para 
+elegir fichero
+*/
+window.onload=main;
+
+function main(){
+    document.getElementById("anadirOtro").addEventListener("click",anadir);
+}
+
+function anadir(){
+    const input=document.createElement("input");
+    input.type="file";
+    const form=document.querySelector("form");
+    const enviar=document.getElementById("enviar");
+    form.insertBefore(input,enviar);
+    const br=document.createElement("br");
+    form.insertBefore(br,enviar);
+}
---
+++ Tema 5 DOM/work1 ej6 visor de imagenes/work1 ej6.js
@@ -0,0 +1,8 @@
+/*
+Se desea realizar mediante DOM un visor de imágenes. Para ello
+en tu página se mostrará una imágen, junto a los botones "Siguiente" y "Anterior".
+Impleméntale la funcionalidad para poder navegar entre un conjunto de imágnees
+que tendrás previamente preparadas
+*/
+
+
