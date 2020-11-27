805ab5f4a2ab95b13dabc1ccdb7395085a73326b
juande <cloud15jc@gmail.com>
2020-11-28 00:22:25 +0100

    cat class done

--- Tema 5 DOM/ej gato/gato.js
+++ Tema 5 DOM/ej gato/gato.js
@@ -1,21 +1,43 @@
-window.onload=main;
-
-function main(){
-    cont=0;
-    let gato1=document.getElementById("imgGato1");
-    gato1.addEventListener("click", click);
-
-    let arr=document.getElementsByTagName("img");
-    for(let i=0; i<arr.length; i++){
-        arr[i].addEventListener("click", () => {
-            cont++;
-
-        });
+class Gato{
+    constructor(nombre,rutaImagen){
+        this.nombre=nombre;
+        this.rutaImagen=rutaImagen;
+        this.contador=0;
     }
 }
-////HACER CLASE GATO, se le pasan parametros y se crea el src, dentro createElement, etc
-function click(){
-    
-    let p1=document.getElementById("p1");
-    p1.innerHTML=cont;
+
+
+var gatos=[];
+var imagen;
+var pContador;
+window.onload=() => {
+    let gato1=new Gato("CT-5555","img/gato1.jpg");
+    let gato2=new Gato("CT-6749", "img/gato2.jpg");
+
+    gatos.push(gato1);
+    gatos.push(gato2);
+
+    imagen=document.querySelector("img");
+    pContador=document.querySelector("p");
+    let li;
+    let ul=document.querySelector("ul");
+    gatos.forEach( (gatito,i)  => {//si le paso otro parametro a forEach, ese indicará el índice
+        li=document.createElement("li");
+        li.appendChild(document.createTextNode(gatito.nombre));
+        li.numGato=i //se pueden crear atributos a cualquier elemento sin mas
+        ul.appendChild(li);
+        li.addEventListener("click",pulsado);
+    });
+}
+
+function pulsado(e){
+    let objetoGato=gatos[e.target.numGato];
+    imagen.src=objetoGato.rutaImagen;
+    imagen.i=e.target.numGato;
+    pContador.textContent=gatos[e.target.numGato].contador;
+    imagen.addEventListener("click",sumaContador);
+}
+function sumaContador(e){
+    gatos[e.target.i].contador++;//esta i es el atributo creado arriba, sobre imagen para que guarde el numero
+    pContador.textContent=gatos[e.target.i].contador;
 }
