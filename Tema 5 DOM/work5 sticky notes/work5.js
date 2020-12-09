bfb0a80da091ec37628f85f10680ddb829e2131d
juande <cloud15jc@gmail.com>
2020-12-09 13:05:43 +0100

    begining the sticky notes

---
+++ Tema 5 DOM/work5 sticky notes/work5.js
@@ -0,0 +1,20 @@
+/* class Notas{
+    contructor(){
+
+    }
+    
+} */
+/* let nota={
+    "Nota":[
+        {
+            "titulo":"as",
+            "texto":"",
+            "tiempo":""
+        }
+    ]
+} */
+
+window.onload=()=>{
+    document.write(nota.Nota[0].titulo);
+
+}
